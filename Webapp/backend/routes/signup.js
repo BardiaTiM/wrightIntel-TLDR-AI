d70841a9ae8c4607173513d8b58ff99fd01770a2
jamesnll <langillejames11@gmail.com>
2023-05-11 11:10:58 -0700

    Added phone number to sign up page as an additional piece of info

--- Webapp/backend/routes/signup.js
+++ Webapp/backend/routes/signup.js
@@ -12,6 +12,7 @@ module.exports = function(userCollection, saltRounds, expireTime) {
   router.post('/submitUser', async (req, res) => {
     var username = req.body.username;
     var email = req.body.email;
+    var phoneNum = req.body.phoneNumber;
     var password = req.body.password;
     // Check for missing fields
     if (!username) {
@@ -22,6 +23,10 @@ module.exports = function(userCollection, saltRounds, expireTime) {
         res.status(400).json({ error: 'Please enter an email' });
         return;
     }
+    if (!phoneNum) {
+      res.status(400).json({ error: 'Please enter a phone number' });
+      return;
+  }
     if (!password) {
         res.status(400).json({ error: 'Please enter a password' });
         return;
@@ -39,11 +44,12 @@ module.exports = function(userCollection, saltRounds, expireTime) {
   const schema = joi.object(
     {
       username: joi.string().alphanum().max(20).required(),
-            email: joi.string().email().required(),
+      email: joi.string().email().required(),
+      phoneNum: joi.string().max(12).required(),
       password: joi.string().max(20).required()
     });
   // Validate user input
-  const validationResult = schema.validate({username, email, password});
+  const validationResult = schema.validate({username, email, phoneNum, password});
   if (validationResult.error != null) {
       console.log(validationResult.error);
       res.status(400).json({ error: 'Potential NoSQL Injection detected.' });
@@ -52,7 +58,7 @@ module.exports = function(userCollection, saltRounds, expireTime) {
     // Bcrypt password
     var hashedPassword = await bcrypt.hash(password, saltRounds);
   // Insert user into database
-  await userCollection.insertOne({username: username, email: email, password: hashedPassword});
+  await userCollection.insertOne({username: username, email: email, phoneNumber: phoneNum, password: hashedPassword});
   console.log("Inserted user");
   // Go to members page
   req.session.authenticated = true;

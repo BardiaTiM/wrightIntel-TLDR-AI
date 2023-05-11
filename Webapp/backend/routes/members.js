53cb812270986ec1414b107bfc01d540fb2150a0
Bardia <bilbeigitimouri@my.bcit.ca>
2023-05-10 18:09:41 -0700

    Able to log in now.

--- Webapp/backend/routes/members.js
+++ Webapp/backend/routes/members.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/members', (req, res) => {
+// Define the images array
+const images = ['image1.jpg', 'image2.jpg', 'image3.jpg'];
+
+router.get('/authenticate', (req, res) => {
   if (!req.session.authenticated) {
     res.status(401).json({ error: 'You are not authorized to view this page.' });
-}
-var image = images[Math.floor(Math.random() * images.length)];
-var imageURL = image;
-res.status(200).json({ message: `Hello, ${req.session.username}!`, image: `${imageURL}}`})
+  } else {
+    var image = images[Math.floor(Math.random() * images.length)];
+    var imageURL = image;
+    res.status(200).json({ message: `Hello, ${req.session.username}!`, image: `${imageURL}}`, authenticated: true});
+  }
 });
 
 module.exports = router;
-

d0e782f6970e94144fe1d85743d65dca30ed163b
chino0522 <beakjinho0522@gmail.com>
2023-05-15 16:58:22 -0700

    login feature is fixed

--- Webapp/index.js
+++ Webapp/index.js
@@ -30,53 +30,53 @@ const email_password = process.env.EMAIL_PASSWORD;
 /** End of secret info. */
 
 /** Database connection. */
-var {database} = require('./databaseConnection.js');
+var { database } = require('./databaseConnection.js');
 
 const userCollection = database.db(mongodb_database).collection('users');
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 var mongoStore = mongoDB.create({
-	mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/Sessions`,
-	crypto: {
-		secret: mongodb_secretSession
-	}
+  mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/Sessions`,
+  crypto: {
+    secret: mongodb_secretSession
+  }
 })
 /** End of database connection. */
 
 app.use(session({
-    secret: nodeSecretSession,
-	store: mongoStore,
-	saveUninitialized: false,
-	resave: true
+  secret: nodeSecretSession,
+  store: mongoStore,
+  saveUninitialized: false,
+  resave: true
 }
 ))
 
 /** Session Validation Functions. */
 function isValidSession(req) {
   if (req.session.authenticated) {
-      return true;
+    return true;
   }
   return false;
 }
 
-function sessionValidation(req,res,next) {
+function sessionValidation(req, res, next) {
   if (isValidSession(req)) {
-      next();
+    next();
   }
   else {
-      res.redirect('/login');
+    res.redirect('/login');
   }
 }
 /** End of session validation functions. */
 
 /** Landing page. */
 app.get('/', (req, res) => {
-    if (!req.session.authenticated) {
-      res.render('index', {req: req});
-    } else {
-      res.redirect('/chatbot')
-    }
+  if (!req.session.authenticated) {
+    res.render('index', { req: req });
+  } else {
+    res.redirect('/chatbot')
+  }
 })
 
 /** Sign up page. */
@@ -86,57 +86,65 @@ app.get('/signup', (req, res) => {
 
 
 /** Sign up validation. */
-app.post('/signupValidation', async (req,res) => {
-    var username = req.body.username;
-    var email = req.body.email;
-    var phoneNum = req.body.phoneNumber;
-    var password = req.body.password;
-    var html;
-    // Check for missing fields
-    if (!username) {
-        html = `<h1>Sign up error</h1><p>Missing username</p><a href='/signup'>Try again</a>`;
-        res.send(html);
-        return;
-    }
-    if (!email) {
-        html = `<h1>Sign up error</h1><p>Missing email</p><a href='/signup'>Try again</a>`;
-        res.send(html);
-        return;
-    }
-    if (!password) {
-        html = `<h1>Sign up error</h1><p>Missing password</p><a href='/signup'>Try again</a>`;
-        res.send(html);
-        return;
-    }
-    if (!phoneNum) {
-        html = `<h1>Sign up error</h1><p>Missing phone number</p><a href='/signup'>Try again</a>`;
-        res.send(html);
-        return;
-    }
-    
-    // Check for noSQL injection attacks
-	const schema = joi.object(
-		{
-			username: joi.string().alphanum().max(20).required(),
+app.post('/signupValidation', async (req, res) => {
+  var name = req.body.name;
+  var username = req.body.username;
+  var email = req.body.email;
+  var phoneNum = req.body.phoneNumber;
+  var password = req.body.password;
+  var html;
+  // Check for missing fields
+  if (!username) {
+    html = `<h1>Sign up error</h1><p>Missing username</p><a href='/signup'>Try again</a>`;
+    res.send(html);
+    return;
+  }
+  if (!email) {
+    html = `<h1>Sign up error</h1><p>Missing email</p><a href='/signup'>Try again</a>`;
+    res.send(html);
+    return;
+  }
+  if (!password) {
+    html = `<h1>Sign up error</h1><p>Missing password</p><a href='/signup'>Try again</a>`;
+    res.send(html);
+    return;
+  }
+  if (!phoneNum) {
+    html = `<h1>Sign up error</h1><p>Missing phone number</p><a href='/signup'>Try again</a>`;
+    res.send(html);
+    return;
+  }
+  if (!name) {
+    html = `<h1>Sign up error</h1><p>Missing name</p><a href='/signup'>Try again</a>`;
+    res.send(html);
+    return;
+  }
+
+  // Check for noSQL injection attacks
+  const schema = joi.object(
+    {
+      name: joi.string().alphanum().max(20).required(),
+      username: joi.string().alphanum().max(20).required(),
       email: joi.string().email().required(),
       phoneNum: joi.string().max(12).required(),
-			password: joi.string().max(20).required()
-		});
-	// Validate user input
-	const validationResult = schema.validate({username, email, phoneNum, password});
-	if (validationResult.error != null) {
-        console.log(validationResult.error);
-        res.status(400).json({ error: 'Potential NoSQL Injection detected.'});
-	   return;
-	}
-    // Bcrypt password
-    var hashedPassword = await bcrypt.hash(password, saltRounds);
-	// Insert user into database
-await userCollection.insertOne({username: username, email: email, password: hashedPassword, phoneNumber: phoneNum});
+      password: joi.string().max(20).required()
+    });
+  // Validate user input
+  const validationResult = schema.validate({ name, username, email, phoneNum, password });
+  if (validationResult.error != null) {
+    console.log(validationResult.error);
+    res.status(400).json({ error: 'Potential NoSQL Injection detected.' });
+    return;
+  }
+  // Bcrypt password
+  var hashedPassword = await bcrypt.hash(password, saltRounds);
+  // Insert user into database
+  await userCollection.insertOne({ name: name, username: username, email: email, password: hashedPassword, phoneNumber: phoneNum });
 
-	console.log("Inserted user");
+  console.log("Inserted user");
 
   // Go to members page
+  req.session.name = name;
   req.session.authenticated = true;
   req.session.username = username;
   req.session.email = email;
@@ -151,34 +159,34 @@ app.get('/login', (req, res) => {
 });
 
 /** Login validation. */
-app.post('/loginValidation', async (req,res) => {
-    var email = req.body.email;
-    var password = req.body.password;
+app.post('/loginValidation', async (req, res) => {
+  var email = req.body.email;
+  var password = req.body.password;
 
-    // Create a joi object to check both email and password
-    const schema = joi.object({
-        email: joi.string().email().required(),
-        password: joi.string().max(20).required()
-    });
-    const validationResult = schema.validate({email, password});
-	if (validationResult.error != null) {
-        console.log(validationResult.error);
-        res.status(400).json({ error: 'Potential NoSQL Injection detected.'});
-	   return;
-	}
-    // Find user details in database from email
-	const result = await userCollection.find({email: email}).project({username: 1, password: 1, phoneNumber: 1, _id: 1}).toArray();
-    
-	console.log(result);
-    // User not found
-	if (result.length != 1) {
-		console.log("user not found");
-		res.redirect("/login");
-		return;
-	}
-    // Check password
-	if (await bcrypt.compare(password, result[0].password)) {
-		console.log("correct password");
+  // Create a joi object to check both email and password
+  const schema = joi.object({
+    email: joi.string().email().required(),
+    password: joi.string().max(20).required()
+  });
+  const validationResult = schema.validate({ email, password });
+  if (validationResult.error != null) {
+    console.log(validationResult.error);
+    res.status(400).json({ error: 'Potential NoSQL Injection detected.' });
+    return;
+  }
+  // Find user details in database from email
+  const result = await userCollection.find({ email: email }).project({ username: 1, password: 1, phoneNumber: 1, _id: 1 }).toArray();
+
+  console.log(result);
+  // User not found
+  if (result.length != 1) {
+    console.log("user not found");
+    res.redirect("/login");
+    return;
+  }
+  // Check password
+  if (await bcrypt.compare(password, result[0].password)) {
+    console.log("correct password");
     // Go to members page
     req.session.authenticated = true;
     req.session.username = result[0].username;
@@ -186,30 +194,30 @@ app.post('/loginValidation', async (req,res) => {
     req.session.phoneNumber = result[0].phoneNumber;
     req.session.cookie.maxAge = expireTime;
     res.redirect("/chatbot");
-		return;
+    return;
     // Incorrect password
-	} else {
-        console.log("incorrect password");
-        html = `<h1>Login error</h1><p>Incorrect password</p><a href='/login'>Try again</a>`;
-        res.send(html);
-        return;
-	}
+  } else {
+    console.log("incorrect password");
+    html = `<h1>Login error</h1><p>Incorrect password</p><a href='/login'>Try again</a>`;
+    res.send(html);
+    return;
+  }
 });
 
 /** Chatbot page. */
 app.get('/chatbot', (req, res) => {
-    const marmot1 = "/marmot1.gif";
-    const marmot2 = "/marmot2.gif";
-    const marmot3 = "/marmot3.gif";
-    res.render('chatbot', {req: req, res: res, username: req.session.username, pic1: marmot1, pic2: marmot2, pic3: marmot3 });
+  const marmot1 = "/marmot1.gif";
+  const marmot2 = "/marmot2.gif";
+  const marmot3 = "/marmot3.gif";
+  res.render('chatbot', { req: req, res: res, username: req.session.username, pic1: marmot1, pic2: marmot2, pic3: marmot3 });
 });
 
 /** Logout page. */
-app.get('/logout', (req,res) => {
+app.get('/logout', (req, res) => {
   req.session.destroy((err) => {
     if (err) {
       console.log(err);
-     } else {
+    } else {
       res.redirect('/');
     }
   });
@@ -217,120 +225,120 @@ app.get('/logout', (req,res) => {
 
 /** Personal profile page. */
 app.get('/profile', (req, res) => {
-  res.render('profile', {req: req, res: res, username: req.session.username, email: req.session.email, phoneNumber: req.session.phoneNumber});
+  res.render('profile', { req: req, res: res, username: req.session.username, email: req.session.email, phoneNumber: req.session.phoneNumber });
 });
 
 
 /** Forgot password page. */
 app.get('/forgot-password', (req, res) => {
-  res.render('forgot-password');
+  res.render('forgot-password', { message: '' });
+});
+
+
+/* ALL BELOW IMPORTANT FOR PASSWORD REST */
+
+
+// Handle forgot password form submission
+app.post('/forgot-password', async (req, res) => {
+
+  // create reusable transporter object using the default SMTP transport
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // use SSL
+    auth: {
+      user: email_user,
+      pass: email_password
+    },
+    tls: {
+      rejectUnauthorized: false
+    }
   });
 
-  
-  /* ALL BELOW IMPORTANT FOR PASSWORD REST */
+  const { email } = req.body;
 
+  // Generate a unique token for the user
+  const token = await bcrypt.hash(email + Date.now(), saltRounds);
 
-  // Handle forgot password form submission
-  app.post('/forgot-password', async (req, res) => {
+  // Store the token in the database along with the user's email and a timestamp
+  await userCollection.updateOne(
+    { email },
+    { $set: { passwordResetToken: token, passwordResetExpires: Date.now() + 3600000 } }
+  );
 
-    // create reusable transporter object using the default SMTP transport
-    const transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
-      port: 465,
-      secure: true, // use SSL
-      auth: {
-          user: email_user,
-          pass: email_password
-      },
-      tls: {
-          rejectUnauthorized: false
-      }
+  // Send an email to the user with a link to the password reset page
+  const resetUrl = `https://tldr-node.onrender.com/reset-password?token=${token}`;
+  const mailOptions = {
+    from: email_user,
+    to: email,
+    subject: 'Reset your password',
+    html: `Click <a href="${resetUrl}">here</a> to reset your password.`
+  };
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Failed to send email');
+    } else {
+      res.status(200).render('forgot-password', { message: 'Email has been sent successfully' })
+    }
   });
-    
-    const { email } = req.body;
-  
-    // Generate a unique token for the user
-    const token = await bcrypt.hash(email + Date.now(), saltRounds);
-  
-    // Store the token in the database along with the user's email and a timestamp
-    await userCollection.updateOne(
-      { email },
-      { $set: { passwordResetToken: token, passwordResetExpires: Date.now() + 3600000 } }
-    );
-  
-    // Send an email to the user with a link to the password reset page
-    const resetUrl = `https://tldr-node.onrender.com/reset-password?token=${token}`;
-    const mailOptions = {
-      from: email_user,
-      to: email,
-      subject: 'Reset your password',
-      html: `Click <a href="${resetUrl}">here</a> to reset your password.`
-    };
-    transporter.sendMail(mailOptions, (err, info) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send('Failed to send email');
-      } else {
-        res.status(200).send('Email sent');
-      }
-    });
-  });
+});
 
-  // Handle password reset form submission
+// Handle password reset form submission
 app.post('/reset-password', async (req, res) => {
-    const { token, password } = req.body;
-  
-    // Find the user with the given token and ensure that the token hasn't expired
-    const user = await userCollection.findOne({
-      passwordResetToken: token,
-      passwordResetExpires: { $gt: Date.now() }
-    });
-  
-    if (!user) {
-      return res.status(400).send('Invalid or expired token');
-    }
-  
-    // Update the user's password
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
-    await userCollection.updateOne(
-      { email: user.email },
-      { $set: { password: hashedPassword } }
-    );
-  
-    // Remove the password reset token and expiration time from the user's record
-    await userCollection.updateOne(
-      { email: user.email },
-      { $unset: { passwordResetToken: 1, passwordResetExpires: 1 } }
-    );
-  
-    res.status(200).send('Password reset successfully');
-  });
-  
-  // Password reset page
-  app.get('/reset-password', async (req, res) => {
-    const { token } = req.query;
-  
-    // Find the user with the given token and ensure that the token hasn't expired
-    const user = await userCollection.findOne({
-      passwordResetToken: token,
-      passwordResetExpires: { $gt: Date.now() }
-    });
-  
-    if (!user) {
-      return res.status(400).send('Invalid or expired token');
-    }
-    res.render('reset-password', { token });
+  const { token, password } = req.body;
+
+  // Find the user with the given token and ensure that the token hasn't expired
+  const user = await userCollection.findOne({
+    passwordResetToken: token,
+    passwordResetExpires: { $gt: Date.now() }
   });
 
-    /* ALL ABOVE IMPORTANT FOR PASSWORD REST */
+  if (!user) {
+    return res.status(400).send('Invalid or expired token');
+  }
+
+  // Update the user's password
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+  await userCollection.updateOne(
+    { email: user.email },
+    { $set: { password: hashedPassword } }
+  );
+
+  // Remove the password reset token and expiration time from the user's record
+  await userCollection.updateOne(
+    { email: user.email },
+    { $unset: { passwordResetToken: 1, passwordResetExpires: 1 } }
+  );
+
+  res.status(200).send('Password reset successfully');
+});
+
+// Password reset page
+app.get('/reset-password', async (req, res) => {
+  const { token } = req.query;
+
+  // Find the user with the given token and ensure that the token hasn't expired
+  const user = await userCollection.findOne({
+    passwordResetToken: token,
+    passwordResetExpires: { $gt: Date.now() }
+  });
+
+  if (!user) {
+    return res.status(400).send('Invalid or expired token');
+  }
+  res.render('reset-password', { token });
+});
+
+/* ALL ABOVE IMPORTANT FOR PASSWORD REST */
 
 
 // 404 page
-app.get("*", (req,res) => {
-	res.render('404', {res: res});
+app.get("*", (req, res) => {
+  res.render('404', { res: res });
 })
 
 // Start server
 app.listen(port, () => {
-    console.log("Node application listening on port " + port);
+  console.log("Node application listening on port " + port);
 })

9a4df4655cfc992e61baae737ab4d42d61d10123
Bardia <bilbeigitimouri@my.bcit.ca>
2023-05-23 15:19:22 -0700

    Completed debugging.

--- Webapp/ChatBot/text.py
+++ Webapp/ChatBot/text.py
@@ -4,7 +4,7 @@ from datetime import datetime
 api_key = 'TaGGXjzKCpT0cZ1dxthulOliuG4zdQrC'
 api_url = 'https://aeroapi.flightaware.com/aeroapi/'
 
-flight_id = 'AC5291'
+flight_id = 'WS3282'
 auth_header = {'x-apikey': api_key}
 
 response = requests.get(api_url + f'flights/{flight_id}', headers=auth_header)

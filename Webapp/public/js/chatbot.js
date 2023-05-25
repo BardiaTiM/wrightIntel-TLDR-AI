98034a36ecd97c3815f6f9e4e29c0bdf5c50193d
Will Ondrik <willondrik@my.bcit.ca>
2023-05-25 14:35:00 -0700

    Corrected the file paths to the new images folder

--- Webapp/public/js/chatbot.js
+++ Webapp/public/js/chatbot.js
@@ -565,6 +565,6 @@ function updateAirlineName(airlineName, formalAirlineName) {
 // Replace the image with the specified source
 function replaceImage() {
   const logoImage = document.getElementById("logo2B");
-  logoImage.src = "logo1B.png";
+  logoImage.src = "./images/chatbot_images/logo1B.png";
   logoImage.style.width = "40%"; // Adjust the width value to the desired size
 }
